982d94fa6889bcf9ba6c96602bba7cb68d6ed279
danielaescutia07 <danielaescutia@yahoo.com>
2021-12-13 18:55:33 -0600

    creted users-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,9 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
+//*Bringing in 'users-model' and 'auth-middleware'
+const express = require('express');
+const router = express.Router();
+const User = require('./users-model');
+const restricted = require('../auth/auth-middleware');
 
 /**
   [GET] /api/users
@@ -24,5 +28,18 @@
   }
  */
 
+router.get('/', (req, res, next) => {
+  User.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(next({
+      status: 401,
+      message: 'You shall not pass!'
+    }))
+})
+
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router;
